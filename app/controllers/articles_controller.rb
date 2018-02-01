ce0b851abd9b7d19b34090e0b22d6aff7c004afc
Subarna Chakrabarty <subarna.chakrabarty94@gmail.com>
2018-02-01 21:33:34 +0000

    a lot of modifications

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,15 @@
 class ArticlesController < ApplicationController 
+  
+  def index 
+    @articles = Article.all
+  end
+  
   def new 
   @article = Article.new 
+  end 
+  
+  def edit 
+   @article = Article.find(params[:id]) 
   end 
   
   def create 
@@ -16,6 +25,18 @@ class ArticlesController < ApplicationController
      
   # article.save
    #redirect_to articles_path(@article)
+  end 
+  
+  def update 
+    
+    @article = Article.find(params[:id]) 
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated."
+      redirect_to articles_path(@article)
+      
+    else 
+      render 'edit'
+    end 
   end 
   
   def show
