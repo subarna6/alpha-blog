55c38a8c389c01fb1ed00a7cc17d83f24360033f
Subarna Chakrabarty <subarna.chakrabarty94@gmail.com>
2018-02-14 22:14:48 +0000

    assosiation successful

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212190258) do
+ActiveRecord::Schema.define(version: 20180214213848) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +24,11 @@ ActiveRecord::Schema.define(version: 20180212190258) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "articles_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
